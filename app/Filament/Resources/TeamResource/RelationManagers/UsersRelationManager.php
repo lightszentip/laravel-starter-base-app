ce2c899eed81bfeefd6f814cd0ef0ba7d4a52db1
Tobi <lightszentip@gmail.com>
2025-01-01 23:31:28 +0100

    feat: add handling for teams

---
+++ app/Filament/Resources/TeamResource/RelationManagers/UsersRelationManager.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Resources\TeamResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Resources\RelationManagers\RelationManager;
+use Livewire\Attributes\On;
+use Livewire\Component;
+
+class UsersRelationManager extends RelationManager
+{
+
+    protected static string $relationship = 'users';
+
+
+
+    public function form(Forms\Form $form): Forms\Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user_id')
+                    ->relationship('users', 'name')
+                    ->required(),
+            ]);
+    }
+
+
+    public function table(Tables\Table $table): Tables\Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Name'),
+                Tables\Columns\TextColumn::make('username')
+                    ->label('Username'),
+                Tables\Columns\TextColumn::make('email')
+                    ->label('Email'),
+            ])
+            ->filters([])
+            ->headerActions([
+            ])
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([]);
+    }
+
+    #[On('record-updated')]
+    public function recordFormUpdated(): void
+    {
+        $this->refreshFormData(['terminated_at', 'approved_at']);
+    }
+
+}
+
